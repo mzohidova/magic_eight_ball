31e737a9313acacf3d35c7dccdee0e2e567ad434
mzohidova <69942297+mzohidova@users.noreply.github.com>
2020-11-18 22:29:26 +0300

    Add files via upload

---
+++ magic_eight_ball.js
@@ -0,0 +1,48 @@
+let userName = 'Matluba';
+
+userName === 'Matluba' ? console.log (`Hello, ${userName}!`) : console.log (`Hello!`);
+
+let userQuestion = 'Will Donald Trump be a president?';
+console.log (`${userName} asked: ${userQuestion}`);
+
+let randomNumber = Math.floor(Math.random()*8);
+
+let eightBall = '';
+
+switch (randomNumber) {
+    case 0:
+        console.log ('It is certain');
+        break;
+    case 1:
+        console.log ('It is decidedly so');
+        break;
+    case 2:
+        console.log ('Reply hazy try again');
+        break;
+    case 3:
+        console.log ('Cannot predict now');
+        break;
+    case 4:
+        console.log ('Do not count on it');
+        break;
+    case 5:
+        console.log ('My sources say no');
+        break;
+    case 6:
+        console.log ('Outlook not so good');
+        break;
+    case 7:
+        console.log ('Signs point to yes');
+        break;
+    default:
+        console.log ('Try again');
+        break;
+}
+
+console.log (eightBall);
+
+
+
+
+
+
